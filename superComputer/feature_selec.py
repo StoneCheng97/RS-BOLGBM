4374e5c3a55ceb87970b4fc91ad4a9fa69ac6b31
stone <673219192@qq.com>
2022-03-21 17:34:43 +0800

    0.4 添加ReliefF

--- superComputer/feature_selec.py
+++ superComputer/feature_selec.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+import ReliefF
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.pipeline import make_pipeline
-from skrebate import ReliefF
+# from skrebate import ReliefF
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 
@@ -16,17 +17,18 @@ raw_data = pd.read_csv("D:\pythonProject\spark\datas\sc\sjtu_2019_new.csv")
 test_data = raw_data.dropna()
 # features = test_data.loc[:, ["Wait_Time", "NAP", "Run_Time", "User_ID", "Queue_Number", "Submit_Time"]]
 # labels = test_data.loc[:, "Status"]
-features, labels = test_data.drop(labels=['Status','RNP'], axis=1).values, test_data['Status'].values
+features, labels = test_data.drop(labels=['Status', 'RNP'], axis=1).values, test_data['Status'].values
 print('特征：', features)
 print("目标：", labels)
 
+ref = ReliefF.ReliefF(n_neighbors=100,n_features_to_keep=5)
+ref.fit(features,labels)
+print(ref.transform(features))
 # skb = SelectKBest(score_func=chi2, k=5)# 去掉了Queue_Number
 # skb.fit(features, labels)
 # print("SelectKBest:", skb.transform(features))
 # print("Scores:",skb.scores_)
-ref = ReliefF(n_features_to_select=2, n_neighbors=100)
-ref.fit(features, labels)
-print(ref.transform(features))
+
 
 # clf = make_pipeline(ReliefF(n_features_to_select=2, n_neighbors=100), RandomForestClassifier(n_estimators=100))
 # print(np.mean(cross_val_score(clf, features, labels)))
